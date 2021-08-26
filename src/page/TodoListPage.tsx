26907c29b30623399f2ac42db882145674bb94ff
Funncy <gywns41@naver.com>
2021-08-26 21:49:03 +0900

    Implement : TodoListPage

---
+++ src/page/TodoListPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TodoListPage.css';
+
+
+function TodoListPage(): JSX.Element {
+  return (
+    <div className='TodoListBody'>
+      hello
+    </div>
+  );
+}
+
+export default TodoListPage;
