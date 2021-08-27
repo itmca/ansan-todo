8766f152eaafa8ac9dc082f4ae550d8db6b568c5
Funncy <gywns41@naver.com>
2021-08-27 23:20:38 +0900

    Implement : TodoListPage Strcuture

--- src/page/TodoListPage.tsx
+++ src/page/TodoListPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './TodoListPage.css';
 import TodoListBody from '../components/TodoListBody';
+import TodoListHead from '../components/TodoListHead';
+import TodoCreate from '../components/TodoCreate';
 
 
 function TodoListPage(): JSX.Element {
   return (
     <div className='TodoListBody'>
+      <TodoListHead/>
       <TodoListBody/>
+      <TodoCreate/>
     </div>
   );
 }
