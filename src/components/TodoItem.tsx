9db76c8eae71b1df355cb39d1927e848f2f42f98
Funncy <gywns41@naver.com>
2021-08-27 23:33:52 +0900

    Implement : doneToggle & deleteTodo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import './TodoItem.css';
 import { MdDone, MdDelete } from 'react-icons/md';
+import { useRecoilState } from 'recoil';
+import { ITodoItem, todoListState } from '../recoil/todoRecoil';
 
 
-type TodoItemProps = {
-  text: string;
-  done?: boolean;
-};
 
-function TodoItem({ text, done }:TodoItemProps) {
+function TodoItem({ id, text, done }:ITodoItem) {
+
+  const [todoList, setTodoList] = useRecoilState<ITodoItem[]>(todoListState);
+
+  const doneToggle = () => {
+    setTodoList(todoList.map((todo: ITodoItem)=> {
+      return todo.id === id ? { ...todo, done: !todo.done } : todo;
+    }));
+  };
+
+  const deleteTodo = () => {
+    setTodoList(todoList.filter((todo: ITodoItem)=> todo.id !== id));
+  };
+
   return ( <div className='itemBody'>
-  <div className={`itemDone ${done && 'complete'}`}> <MdDone/></div>
+  <div className={`itemDone ${done && 'complete'}`} onClick={doneToggle}> <MdDone/></div>
   <div className='itemText'>{text}</div>
-  <div className='itemRemove'>
+  <div className='itemRemove' onClick={deleteTodo}>
     <MdDelete/>
   </div>
   </div>);
