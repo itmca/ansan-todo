170aa34e9b53f4f55380dd9cf4dfcffcf9d4e854
Funncy <gywns41@naver.com>
2021-08-27 23:20:28 +0900

    Add : TodoListBody recoilState

--- src/components/TodoListBody.tsx
+++ src/components/TodoListBody.tsx
@@ -1,14 +1,20 @@
 
 
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import todoListState, { ITodoItem } from '../recoil/todoRecoil';
 import TodoItem from './TodoItem';
+import './TodoListBody.css';
 
 function TodoListBody() : JSX.Element{
+
+  const todoList = useRecoilValue<ITodoItem[]>(todoListState);
+  
   return (
     <div className='listBody'>
-      <TodoItem text={'test'}/>
+      {todoList.map((todo)=> <TodoItem text={todo.text} key={todo.id} done={todo.done}/>)}
     </div>
   );
 }
 
-export default TodoListBody;
+export default TodoListBody;  
